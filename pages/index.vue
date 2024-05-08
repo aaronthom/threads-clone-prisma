712a9803305eb1b08a4f9404ba0b38a7687c1917
Aaron Hampe <hampeaaron@gmail.com>
2024-05-08 16:08:23 +0200

    progress

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,36 @@
 <template>
-    <div>
-        INDEX
-    </div>
+    <MainLayout>
+        <div id="IndexPage" class="w-full overflow-auto">
+            <div class="mx-auto max-w-[500px] overflow-hidden">
+                <div id="Posts" class="px-4 max-w-[600px] mx-auto">
+
+                    <div class="text-white" v-if="isPosts" v-for="post in posts" :key="post">
+                        {{ post }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </MainLayout>
 </template>
 
 <script setup>
+import MainLayout from '~/layouts/MainLayout.vue';
+import { useUserStore } from '~/stores/user.js';
+const userStore = useUserStore()
+//const user = useSupabaseUser()
 
+let posts = ref([])
+let isPosts = ref(true)
+let isLoading = ref(false)
+
+onBeforeMount(() => {
+    posts.value = [
+        {
+            name: 'John Weeks Dev',
+            image: 'https://placehold.co/100',
+            text: 'This is the title',
+            picture: 'https://placehold.co/500',
+        }
+    ]
+})
 </script>
